bec8771b0ceaee50462fbd991352cafb63fb4ba6
Arild Berggren <abe@itverket.no>
2022-08-17 11:29:49 +0200

    Display images

--- react-native/screens/TabOneScreen.tsx
+++ react-native/screens/TabOneScreen.tsx
@@ -1,32 +1,58 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { Button, Image, Linking, ScrollView, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { useRetrieveEmployees } from "../hooks/useRetrieveEmployees";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const employeeResult = useRetrieveEmployees();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {employeeResult.error ? (
+        <Text style={styles.title}>{employeeResult.error}</Text>
+      ) : employeeResult.loading ? (
+        <Text style={styles.title}>{"Laster..."}</Text>
+      ) : (
+        <View style={styles.imageContainer}>
+          {employeeResult.employees?.map((employee, index) => {
+            return (
+              <Image
+                style={styles.image}
+                key={index}
+                source={{ uri: employee.Image }}
+              />
+            );
+          })}
+        </View>
+      )}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {},
+  imageContainer: {
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    width: "100%",
+    flexDirection: "row",
+  },
+  image: {
+    width: 100,
+    height: 130,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
