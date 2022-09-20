a2ebe87587cead1d590a750292b910097bdca99b
magnetenstad <tenstad.magne@gmail.com>
2022-09-20 21:33:54 +0200

    Cannot guess same twice

--- react-native/components/GuessInput.tsx
+++ react-native/components/GuessInput.tsx
@@ -1,32 +1,36 @@
-import React, { useEffect } from "react";
-import { useRef } from "react";
-import {StyleSheet, TextInput, Text, View} from "react-native";
+import React, { useEffect, useState } from "react";
+import {StyleSheet, TextInput, Text, View, FlatList} from "react-native";
 import { isName } from "../hooks/names";
 const FONT_SIZE = 48
 export const GuessInput = (props: {onInput: (guess: string) => void, secret: string}) => {
 
-  let inputRef = React.createRef<TextInput>();
-  const checkText = (guess: string) => {
-    if (guess.length == props.secret.length) {
-      
-      if (isName(guess)) {
-        props.onInput(guess);
-        // Clear TextInput
-        inputRef.current!.clear();
-      }
-      
-      
-    }
-  }
+  const [text, setText] = useState('')
+  const [prevGuesses, setPrevGuesses] = useState([''])
+  const [messages, setMessages] = useState([''])
 
   useEffect(() => {
-    inputRef.current!.setNativeProps({
-      style: {width: 0.61 * FONT_SIZE * props.secret.length}
-    })
-  }, [props]);
+    setPrevGuesses([])
+  }, [props.secret]);
 
-  return (
-    <TextInput ref={inputRef} style={styles.input} placeholder="write ur guess" onChangeText={checkText} maxLength={props.secret.length}/>
+  useEffect(() => {
+    if (text.length != props.secret.length) return
+    setText('')
+    if (prevGuesses.includes(text)) {
+      setMessages((messages) => [`${text} has already been guessed!`, ...messages])
+      return
+    }
+    if (!isName(text)) return
+    setPrevGuesses((prevGuesses) => [...prevGuesses, text])
+    props.onInput(text);
+  }, [text])
+
+  return (<View>
+    <TextInput value={text} style={styles.input} placeholder="write ur guess" onChangeText={setText} maxLength={props.secret.length}/>
+    <FlatList
+      data={messages}
+      renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
+    ></FlatList>
+  </View>
   );
 };
 
@@ -39,5 +43,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     fontSize: FONT_SIZE,
     fontFamily: 'courier', // changing font will break it
+    width: 100,
+  },
+  text: {
+    color: 'green' 
   }
 });
